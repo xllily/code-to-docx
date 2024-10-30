8483750f634750d126f0f38243544f2d79dc9dfc
liki.shi <xllily.slikij@gmail.com>
2024-10-30 11:48:21 +0800

    fix version

--- src/utils/common.mjs
+++ src/utils/common.mjs
@@ -3,8 +3,20 @@ import path from 'path';
 /**
 * @returns {string} 版本号
 */
+
 export function getVersion() {
-    const packagePath = path.resolve(process.cwd(), 'package.json');
+    const packagePath = findRootPackageJson(process.cwd());
     const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf-8'));
     return packageJson.version;
+}
+function findRootPackageJson(dir) {
+    const packagePath = path.join(dir, 'package.json');
+    if (fs.existsSync(packagePath)) {
+        return packagePath;
+    }
+    const parentDir = path.dirname(dir);
+    if (parentDir === dir) {
+        throw new Error('package.json not found in any parent directory');
+    }
+    return findRootPackageJson(parentDir);
 }
